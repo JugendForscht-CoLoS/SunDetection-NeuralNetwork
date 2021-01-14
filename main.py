f6eba420e85bde658dcc917d24e844bda282fdc1
Riemann01 <62616772+Riemann01@users.noreply.github.com>
2021-01-14 16:51:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ import tensorflow as tf
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.layers import MaxPooling2D, UpSampling2D, Conv2D, Conv2DTranspose, Input, Add, Concatenate
 from tensorflow.keras.models import Model
+from tensorflow_examples.models.pix2pix import pix2pix
 
 import numpy as np
 import matplotlib.pyplot as plt
 #from IPython.display import clear_output
 
 suns, info = tfds.load('sun_dataset',with_info=True)
-PICTURE_SIZE = 224
+PICTURE_SIZE = 128
 
 def load_images(datapoint):
   input_image = tf.image.resize(datapoint['image'], (PICTURE_SIZE, PICTURE_SIZE))
@@ -24,8 +25,7 @@ def load_images(datapoint):
   return input_image, input_mask
 
 TRAIN_LENGTH = info.splits['train'].num_examples
-BATCH_SIZE = 16
-BUFFER_SIZE = 1000
+BATCH_SIZE = 8
 STEPS_PER_EPOCH = TRAIN_LENGTH // BATCH_SIZE
 
 print(TRAIN_LENGTH)
@@ -45,85 +45,163 @@ def _to_ndarray(data):
 
     return img, mask
 
-train_images, train_masks = _to_ndarray(suns['train'])
-test_images, test_masks = _to_ndarray(suns['test'])
-test = (test_images, test_masks)
+#train_images, train_masks = _to_ndarray(suns['train'])
+#test_images, test_masks = _to_ndarray(suns['test'])
+#test = (test_images, test_masks)
 
-test2 = suns['test'].map(load_images)
+train = suns['train'].map(load_images).batch(BATCH_SIZE)
+test = suns['test'].map(load_images).batch(BATCH_SIZE)
 
 data_gen_args = dict(horizontal_flip=True,
                      rotation_range=15,
                      zoom_range=0.3,
                      vertical_flip=True)
-train_images_datagen = ImageDataGenerator(**data_gen_args)
-train_masks_datagen = ImageDataGenerator(**data_gen_args)
+#train_images_datagen = ImageDataGenerator(**data_gen_args)
+#train_masks_datagen = ImageDataGenerator(**data_gen_args)
 
 SEED = 1
-train_images_datagen = train_images_datagen.flow(train_images, batch_size=BATCH_SIZE, shuffle=True, seed=SEED)
-train_masks_datagen = train_masks_datagen.flow(train_masks, batch_size=BATCH_SIZE, shuffle=True, seed=SEED)
+#train_images_datagen = train_images_datagen.flow(train_images, batch_size=BATCH_SIZE, shuffle=True, seed=SEED)
+#train_masks_datagen = train_masks_datagen.flow(train_masks, batch_size=BATCH_SIZE, shuffle=True, seed=SEED)
 
 def image_mask_generator(image_data_generator, mask_data_generator):
     train_generator = zip(image_data_generator, mask_data_generator)
     for (img, mask) in train_generator:
         yield (img, mask)
 
-train_generator = image_mask_generator(train_images_datagen, train_masks_datagen)
+#train_generator = image_mask_generator(train_images_datagen, train_masks_datagen)
 
-sample_image, sample_mask = next(train_generator)
+#sample_image, sample_mask = next(train_generator)
 
-OUTPUT_CHANNELS = 3
-
+OUTPUT_CHANNELS = 2
+'''ACTIVATION = 'elu'
 
 inputs = Input(shape=(PICTURE_SIZE, PICTURE_SIZE, 3))
 def downsample(input, filters=8, third_conv=False, padding='valid'):
-  model = Conv2D(filters=filters, kernel_size=[3,3], activation='relu', padding=padding)(input)
-  model = Conv2D(filters=(filters+4), kernel_size=[3,3], activation='relu', padding=padding)(model)
+  model = Conv2D(filters=filters, kernel_size=[3,3], activation=ACTIVATION, padding=padding)(input)
+  model = Conv2D(filters=(filters+4), kernel_size=[3,3], activation=ACTIVATION, padding=padding)(model)
   if third_conv:
-    model = Conv2D(filters=(filters+8), kernel_size=[3,3], activation='relu', padding=padding)(model)
+    model = Conv2D(filters=(filters+8), kernel_size=[3,3], activation=ACTIVATION, padding=padding)(model)
 
   model = MaxPooling2D(pool_size=[2,2])(model)
   return model
 
 def upsample(input, filters=20, third_conv=False):
   model = UpSampling2D(size=[2,2])(input)
-  model = Conv2DTranspose(filters=filters, kernel_size=[3,3], activation='relu')(model)
+  model = Conv2DTranspose(filters=filters, kernel_size=[3,3], activation=ACTIVATION)(model)
   if third_conv:
-    model = Conv2DTranspose(filters=filters-4, kernel_size=[3,3], activation='relu')(model)
+    model = Conv2DTranspose(filters=filters-4, kernel_size=[3,3], activation=ACTIVATION)(model)
     filters-=4
-  model = Conv2DTranspose(filters=filters-4, kernel_size=[3,3], activation='relu')(model)
+  model = Conv2DTranspose(filters=filters-4, kernel_size=[3,3], activation=ACTIVATION)(model)
   return model
 
 def upsample2(input, filters):
   model = UpSampling2D(size=[2,2])(input)
-  model = Conv2DTranspose(filters=filters, kernel_size=[3,3], activation='relu', padding='same')(model)
+  model = Conv2DTranspose(filters=filters, kernel_size=[3,3], activation=ACTIVATION, padding='same')(model)
   return model
 
-model = downsample(inputs, padding='same')
-output1 = downsample(model, filters=16, padding='same')
-output2 = downsample(output1, filters=32, third_conv=True, padding='same')
-output3 = downsample(output2, filters=64, padding='same')
-model = downsample(output3, filters=128, third_conv=True, padding='same')
+PADDING='valid'
+
+model = downsample(inputs, padding=PADDING)
+model = downsample(model, filters=16, third_conv=True, padding=PADDING)
+output2 = downsample(model, filters=32, padding=PADDING) #thirconv
+output3 = downsample(output2, filters=64, padding=PADDING)
+model = downsample(output3, filters=128, third_conv=True, padding=PADDING)
+
 
 model = upsample2(model, 68)
 model = Concatenate()([model, output3])
 model = upsample2(model, 40)
 model = Concatenate()([model, output2])
-model = upsample2(model, 20)
-model = Concatenate()([model, output1])
+model = upsample2(model, 32)
 model = upsample2(model, 16)
 model = upsample2(model, 8)
-'''
-model = upsample(model, filters=136, third_conv=True)
-model = upsample(model, filters=72)
+
+model = upsample(model, filters=76, third_conv=True) #thirdconv
+model = Add()([model, output3])
 model = upsample(model, filters=40)
-model = upsample(model, filters=24, third_conv=True)
+model = Add()([model, output2])
+model = upsample(model, filters=32)
+model = upsample(model, filters=16, third_conv=True)
 model = upsample(model)
-'''
+
 output = Conv2DTranspose(filters=OUTPUT_CHANNELS, kernel_size=[3,3], activation='softmax', padding='same')(model)
-model = Model(inputs, output)
-ADAM = tf.keras.optimizers.Adam(learning_rate=0.1)
-model.compile(optimizer=ADAM,
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+model = Model(inputs, output)'''
+
+#Start of cnn
+base_model = tf.keras.applications.MobileNetV2(input_shape=[PICTURE_SIZE, PICTURE_SIZE, 3], include_top=False)
+
+# Use the activations of these layers
+layer_names = [
+    'block_1_expand_relu',   # 64x64
+    'block_3_expand_relu',   # 32x32
+    'block_6_expand_relu',   # 16x16
+    'block_13_expand_relu',  # 8x8
+    'block_16_project',      # 4x4
+]
+layers = [base_model.get_layer(name).output for name in layer_names] #Die layers von layer_names bekommen die Ausgabe des MobileNetV2
+
+# Create the feature extraction model
+down_stack = tf.keras.Model(inputs=base_model.input, outputs=layers)
+#Kann nicht trainiert werden
+down_stack.trainable = False
+
+up_stack = [
+    pix2pix.upsample(512, 3),  # 4x4 -> 8x8
+    pix2pix.upsample(256, 3),  # 8x8 -> 16x16
+    pix2pix.upsample(128, 3),  # 16x16 -> 32x32
+    pix2pix.upsample(64, 3),   # 32x32 -> 64x64
+]
+
+def unet_model(output_channels):
+  inputs = tf.keras.layers.Input(shape=[PICTURE_SIZE, PICTURE_SIZE, 3])
+  x = inputs
+
+  # Downsampling through the model
+  skips = down_stack(x)
+  x = skips[-1]
+  skips = reversed(skips[:-1])
+
+  # Upsampling and establishing the skip connections
+  for up, skip in zip(up_stack, skips):
+    x = up(x)
+    concat = tf.keras.layers.Concatenate()
+    x = concat([x, skip])
+
+  # This is the last layer of the model
+  last = tf.keras.layers.Conv2DTranspose(
+      output_channels, 3, strides=2,
+      padding='same')  #64x64 -> 128x128
+
+  x = last(x)
+
+  return tf.keras.Model(inputs=inputs, outputs=x)
+
+model = unet_model(OUTPUT_CHANNELS)
+
+print(model.summary())
+
+from tensorflow.keras import backend as K
+def jaccard_distance_loss(y_true, y_pred, smooth=100):
+    """
+    Jaccard = (|X & Y|)/ (|X|+ |Y| - |X & Y|)
+            = sum(|A*B|)/(sum(|A|)+sum(|B|)-sum(|A*B|))
+    
+    The jaccard distance loss is usefull for unbalanced datasets. This has been
+    shifted so it converges on 0 and is smoothed to avoid exploding or disapearing
+    gradient.
+    
+    Ref: https://en.wikipedia.org/wiki/Jaccard_index
+    
+    @url: https://gist.github.com/wassname/f1452b748efcbeb4cb9b1d059dce6f96
+    @author: wassname
+    """
+    intersection = K.sum(K.abs(y_true * y_pred), axis=-1)
+    sum_ = K.sum(K.abs(y_true) + K.abs(y_pred), axis=-1)
+    jac = (intersection + smooth) / (sum_ - intersection + smooth)
+    return (1 - jac) * smooth
+
+model.compile(optimizer='adam',
+              loss=dice_coef_loss,
               metrics=['accuracy'])
 
 #tf.keras.utils.plot_model(model, show_shapes=True)
@@ -162,11 +240,11 @@ class DisplayCallback(tf.keras.callbacks.Callback):
     show_predictions()
     print ('\nSample Prediction after epoch {}\n'.format(epoch+1))
 '''
-EPOCHS = 20
+EPOCHS = 5
 VAL_SUBSPLITS = 1
 VALIDATION_STEPS = info.splits['test'].num_examples//BATCH_SIZE//VAL_SUBSPLITS
 
-model_history = model.fit(train_generator, epochs=EPOCHS,
+model_history = model.fit(train, epochs=EPOCHS,
                           steps_per_epoch=STEPS_PER_EPOCH,
                           validation_steps=VALIDATION_STEPS,
                           validation_data=test)
@@ -190,5 +268,5 @@ plt.ylim([0, 1])
 plt.legend()
 plt.show()
 
-show_predictions(test2, 6)
+show_predictions(test, 6)
 model.save('neuronalnet/sun_detection.h5')
