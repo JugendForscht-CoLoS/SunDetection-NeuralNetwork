acc5c6794e39a57407c3df91ffa0875191f8d00e
Riemann01 <62616772+Riemann01@users.noreply.github.com>
2021-01-18 15:18:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ def image_mask_generator(image_data_generator, mask_data_generator):
 #sample_image, sample_mask = next(train_generator)
 
 OUTPUT_CHANNELS = 2
-'''ACTIVATION = 'elu'
+ACTIVATION = 'elu'
 
 inputs = Input(shape=(PICTURE_SIZE, PICTURE_SIZE, 3))
 def downsample(input, filters=8, third_conv=False, padding='valid'):
@@ -125,58 +125,7 @@ model = upsample(model, filters=16, third_conv=True)
 model = upsample(model)
 
 output = Conv2DTranspose(filters=OUTPUT_CHANNELS, kernel_size=[3,3], activation='softmax', padding='same')(model)
-model = Model(inputs, output)'''
-
-#Start of cnn
-base_model = tf.keras.applications.MobileNetV2(input_shape=[PICTURE_SIZE, PICTURE_SIZE, 3], include_top=False)
-
-# Use the activations of these layers
-layer_names = [
-    'block_1_expand_relu',   # 64x64
-    'block_3_expand_relu',   # 32x32
-    'block_6_expand_relu',   # 16x16
-    'block_13_expand_relu',  # 8x8
-    'block_16_project',      # 4x4
-]
-layers = [base_model.get_layer(name).output for name in layer_names] #Die layers von layer_names bekommen die Ausgabe des MobileNetV2
-
-# Create the feature extraction model
-down_stack = tf.keras.Model(inputs=base_model.input, outputs=layers)
-#Kann nicht trainiert werden
-down_stack.trainable = False
-
-up_stack = [
-    pix2pix.upsample(512, 3),  # 4x4 -> 8x8
-    pix2pix.upsample(256, 3),  # 8x8 -> 16x16
-    pix2pix.upsample(128, 3),  # 16x16 -> 32x32
-    pix2pix.upsample(64, 3),   # 32x32 -> 64x64
-]
-
-def unet_model(output_channels):
-  inputs = tf.keras.layers.Input(shape=[PICTURE_SIZE, PICTURE_SIZE, 3])
-  x = inputs
-
-  # Downsampling through the model
-  skips = down_stack(x)
-  x = skips[-1]
-  skips = reversed(skips[:-1])
-
-  # Upsampling and establishing the skip connections
-  for up, skip in zip(up_stack, skips):
-    x = up(x)
-    concat = tf.keras.layers.Concatenate()
-    x = concat([x, skip])
-
-  # This is the last layer of the model
-  last = tf.keras.layers.Conv2DTranspose(
-      output_channels, 3, strides=2,
-      padding='same')  #64x64 -> 128x128
-
-  x = last(x)
-
-  return tf.keras.Model(inputs=inputs, outputs=x)
-
-model = unet_model(OUTPUT_CHANNELS)
+model = Model(inputs, output)
 
 print(model.summary())
 
