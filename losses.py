fdbb8cc6c357a5b9a0319413712850b7291c3a7c
Riemann01 <62616772+Riemann01@users.noreply.github.com>
2021-01-23 18:33:34 +0100

    Update losses.py

--- losses.py
+++ losses.py
@@ -4,50 +4,55 @@ import tensorflow_addons as tfa
 from tensorflow.keras import backend as K
 import matplotlib.pyplot as plt
 
-pred = np.zeros(shape=(8, 8, 1))
-true = np.array([np.zeros(shape=(8, 1)),
-                 np.zeros(shape=(8, 1)),
-                 np.zeros(shape=(8, 1)),
-                 [[0],[0],[0],[1],[0],[0],[0],[0]],
-                 [[0],[0],[1],[1],[1],[0],[0],[0]],
-                 [[0],[0],[0],[1],[0],[0],[0],[0]],
-                 np.zeros(shape=(8, 1)),
-                 np.zeros(shape=(8, 1)),
-                 ])
+SIZE=8
+pred = np.zeros(shape=(SIZE, SIZE, 1))
+true = np.zeros(shape=(SIZE, SIZE, 1))
+
+for h in range(0, SIZE):
+    for w in range(0, SIZE):
+        if 0.02>np.random.rand():
+            pred[h][w][0]=1
+
+sun = [SIZE/2-SIZE/20, SIZE/2+SIZE/20]
+for h in range(0, SIZE):
+    for w in range(0, SIZE):
+        if sun[0]<h<sun[1] and sun[0]<w<sun[1]:
+            true[h][w][0]=1
 
 pred = tf.constant(pred, dtype='float32')
 true = tf.constant(true, dtype='float32')
 
-display_list = [true, pred]
-title = ['true', 'predicted']
+def display(display_list):
+    title = ['true', 'predicted']
 
-for i in range(len(display_list)):
-    plt.subplot(1, len(display_list), i+1)
-    plt.title(title[i])
-    plt.imshow(tf.keras.preprocessing.image.array_to_img(display_list[i]))
-    plt.axis('off')
-plt.show()
+    for i in range(len(display_list)):
+        plt.subplot(1, len(display_list), i+1)
+        plt.title(title[i])
+        plt.imshow(tf.keras.preprocessing.image.array_to_img(display_list[i]))
+        plt.axis('off')
+    plt.show()
+
+ones = tf.constant(np.ones((SIZE, SIZE, 1)),dtype='float32')
+inverted_pred = tf.math.subtract(ones, pred)
+inverted_true = tf.math.subtract(ones, true)
+
+display([true, pred])
+display([inverted_true, inverted_pred])
 
 #Crossentropy
-bce = tf.keras.losses.BinaryCrossentropy()
-print('Binary Crossentropy')
-print(bce(true, pred).numpy())
-print()
+# bce = tf.keras.losses.BinaryCrossentropy()
+# print('Binary Crossentropy')
+# print(bce(true, pred).numpy())
+# print()
 
-sfce0 = tfa.losses.SigmoidFocalCrossEntropy(alpha=0.25)
-sfce1 = tfa.losses.SigmoidFocalCrossEntropy(alpha=0.75)
-sfce2 = tfa.losses.SigmoidFocalCrossEntropy(alpha=1.5)
-sfce3 = tfa.losses.SigmoidFocalCrossEntropy(alpha=2.5)
-#gamma hat keine Auswirkung auf die schwere des Losses
-
-print('Sigmoid Focal Crossentropy (Alpha=0.25)')
-print(sfce0(true, pred).numpy())
-print('Sigmoid Focal Crossentropy (Alpha=0.75)')
-print(sfce1(true, pred).numpy())
-print('Sigmoid Focal Crossentropy (Alpha=1.5)')
-print(sfce2(true, pred).numpy())
-print('Sigmoid Focal Crossentropy (Alpha=2.5))')
-print(sfce3(true, pred).numpy())
-print()
+#find max value for alpha 
+for i in range(0,10):
+    i /= 40
+    sfce = tfa.losses.SigmoidFocalCrossEntropy(alpha=i)
+    print('Sigmoid Focal Crossentropy (alpha=',i,')')
+    print(sfce(true, pred).numpy())
+    print()
+    print(sfce(inverted_true, inverted_pred).numpy())
+    print()
 
 #Dice Loss
