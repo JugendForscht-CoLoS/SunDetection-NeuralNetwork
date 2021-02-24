db9881a4c8183bb6ff996dc183b5e8ca6016bfe8
Riemann01 <62616772+Riemann01@users.noreply.github.com>
2021-02-24 18:19:51 +0100

    Update coremlConverter.py

--- CoreMLConverter/coremlConverter.py
+++ CoreMLConverter/coremlConverter.py
@@ -1,15 +1,22 @@
 import coremltools as ct
 import tensorflow as tf
 
-pathH5 = input('Pfad des .h5 models: ')
+from model.model import getModel
+from losses.losses import TverskyLoss
+
 pathML = input('Pfad des .mlmodels: ')
 author = input('Autor: ')
 modelLicense = input('Lizenz: ')
 description = input('kurze Beschreibung: ')
 version = input('Version: ')
 
-keras_model = tf.keras.models.load_model(pathH5)
-mlModel = ct.convert(keras_model)
+model = getModel()
+model.compile(optimizer=tf.keras.optimizers.Adam(lr=0.0005),
+              loss=TverskyLoss(alpha=0.9835, smooth=2e4),
+              metrics=['acc'])
+model.load_weights('neuronalnet/saved_weights)
+
+mlModel = ct.convert(model)
 mlModel.author = author
 mlModel.license = modelLicense
 mlModel.short_description = description
