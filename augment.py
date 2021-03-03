52ae3d4d46ef2d68b7bcdef669ad64261f7ecb6e
Riemann01 <62616772+Riemann01@users.noreply.github.com>
2021-03-03 18:41:06 +0100

    Update augment.py

--- augment.py
+++ augment.py
@@ -6,17 +6,15 @@ import tensorflow as tf
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import matplotlib.pyplot as plt
 
+#Datensatz laden
 suns, info = tfds.load('sun_dataset',with_info=True)
 PICTURE_SIZE = 512
 
+#Datensatz preparieren
 def load_images(datapoint):
   input_image = tf.image.resize(datapoint['image'], (PICTURE_SIZE, PICTURE_SIZE))
   input_mask = tf.image.resize(datapoint['segmentation_mask'], (PICTURE_SIZE, PICTURE_SIZE), method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
-
-  #normalize
-  #input_image = tf.cast(input_image, tf.float32) / 255.0
-  #input_mask = tf.cast(input_mask, tf.float32) / 255.0
-
+  
   return input_image, input_mask
 
 TRAIN_LENGTH = info.splits['train'].num_examples
@@ -24,6 +22,7 @@ BATCH_SIZE = 4
 
 print(info.splits['test'].num_examples)
 
+#Bilder zu numpy-Arrays konvertieren
 def _to_ndarray(data):
     img = np.array([])
     mask = np.array([])
@@ -38,18 +37,22 @@ def _to_ndarray(data):
 
     return img, mask
 
+
 train_images, train_masks = _to_ndarray(suns['train'])
 test_images, test_masks = _to_ndarray(suns['test'])
 
-data_gen_args = dict(horizontal_flip=True,
-                     rotation_range=30)
+#DataGenerator vorbereiten
+data_gen_args = dict(horizontal_flip=True,    #die Bilder sollen automatisch horizontal gespiegelt werden
+                     rotation_range=30)       #und zufällig gedreht  
 test_images_datagen = ImageDataGenerator(**data_gen_args)
 test_masks_datagen = ImageDataGenerator(**data_gen_args)
 
-SEED = 1
+# Die Daten werden nun in den DataGenerator eingegeben. Zusätzlich wird das Verzeichnis eingegeben, wo die Bilder und Masken gespeichert werden sollen 
+SEED = 1  #Der seed sorgt dafür, dass die Maske und das Sonnenbild gleich verändert werden
 test_images_datagen = test_images_datagen.flow(test_images, batch_size=BATCH_SIZE, shuffle=True, seed=SEED,save_to_dir='C:/Users/Jonas/Documents/JugendForscht/test/image', save_format='jpeg', save_prefix='aug')
 test_masks_datagen = test_masks_datagen.flow(test_masks, batch_size=BATCH_SIZE, shuffle=True, seed=SEED,save_to_dir='C:/Users/Jonas/Documents/JugendForscht/test/mask', save_format='jpeg', save_prefix='aug')
 
+#Gleiches Vorgehen 
 data_gen_args = dict(horizontal_flip=True,
                      rotation_range=30)
 train_images_datagen = ImageDataGenerator(**data_gen_args)
@@ -59,12 +62,13 @@ SEED = 2
 train_images_datagen = train_images_datagen.flow(train_images, batch_size=BATCH_SIZE, shuffle=True, seed=SEED, save_to_dir='C:/Users/Jonas/Documents/JugendForscht/test/image', save_format='jpeg', save_prefix='augm')
 train_masks_datagen = train_masks_datagen.flow(train_masks, batch_size=BATCH_SIZE, shuffle=True, seed=SEED, save_to_dir='C:/Users/Jonas/Documents/JugendForscht/test/mask', save_format='jpeg', save_prefix='augm')
 
-
+#die beiden Datagenerator (welche entweder Masken oder Sonnenbilder beinhalten) werden vereint
 def image_mask_generator(image_data_generator, mask_data_generator):
     train_generator = zip(image_data_generator, mask_data_generator)
     for (img, mask) in train_generator:
         yield (img, mask)
 
+#Beim Iterieren durch die train- und test-Generator werden die Bilder gespeichert
 test_datagen = image_mask_generator(test_images_datagen, test_masks_datagen)
 for i in range(0, info.splits['test'].num_examples):
     batch = next(test_datagen)
@@ -72,74 +76,3 @@ for i in range(0, info.splits['test'].num_examples):
 train_datagen = image_mask_generator(train_images_datagen, train_masks_datagen)
 for i in range(0, info.splits['train'].num_examples):
     batch = next(train_datagen)
-
-'''
-path = 'C:/Users/Jonas/tensorflow_datasets/downloads/manual/suns/'
-mpath = (path + 'test/mask')
-ipath = (path + 'test/image')
-
-# we create two instances with the same arguments
-data_gen_args = dict(horizontal_flip=True,
-                     vertical_flip=True,
-                     rescale=1./255)
-train_image_datagen = ImageDataGenerator(**data_gen_args)
-train_mask_datagen = ImageDataGenerator(**data_gen_args)
-# Provide the same seed and keyword arguments to the fit and flow methods
-SEED = 1
-image_generator = train_image_datagen.flow_from_directory(
-    ipath,
-    target_size=(128,128),
-    class_mode=None,
-    interpolation='bicubic',
-    shuffle=False,
-    seed=SEED)
-mask_generator = train_mask_datagen.flow_from_directory(
-    mpath,
-    target_size=(128,128),
-    class_mode=None,
-    interpolation='nearest',
-    shuffle=False,
-    seed=SEED)
-
-
-
-def image_mask_generator(image_data_generator, mask_data_generator):
-    train_generator = zip(image_data_generator, mask_data_generator)
-    for (img, mask) in train_generator:
-        yield (img, mask)
-test_generator = image_mask_generator(image_generator, mask_generator)
-print(len(list(test_generator)))
-
-for image, mask in test_generator.take(12):
-    display(image, mask)
-
-'''
-'''
-i = -1
-temp_img = None
-for img in os.listdir(path):
-    if i % 2 != 0:
-        temp_img = img
-    else:
-        print("Image", str(int(i/2)))
-        print(path + temp_img)
-        print(path + img)
-        print()
-
-    i += 1
-'''
-
-'''
-MANUAL_DOWNLOAD_INSTRUCTIONS=""" dsfghggfh fgh"""
-dl_manager = tfds.download.DownloadManager(download_dir="C:/Users/Jonas/Downloads/", manual_dir="C:/Users/Jonas/tensorflow_datasets/download/manual")
-for filename in os.listdir(dl_manager.manual_dir / "suns/train"):
-    print(os.fsdecode(filename))
-'''
-
-'''dl_manager = tfds.download.DownloadManager(download_dir="C:/Users/Jonas/Downloads/")
-iterator = dl_manager.iter_archive('C:/Users/Jonas/tensorflow_datasets/downloads/manual/suns.zip')
-
-for filename, fobj in iterator:
-    print(filename)    
-    print(fobj.read())
-'''
