19fe581297a9e939cce77058440cbae35225f06c
Riemann01 <62616772+Riemann01@users.noreply.github.com>
2021-03-03 20:29:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,21 +10,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 from IPython.display import clear_output
 
+#Dataset laden
 suns, info = tfds.load('sun_dataset',with_info=True)
 PICTURE_SIZE = 224
 
+#Daten aufbereiten
 @tf.autograph.experimental.do_not_convert
 def load_images(datapoint):
   input_image = tf.image.resize(datapoint['image'], (PICTURE_SIZE, PICTURE_SIZE))
   input_mask = tf.image.resize(datapoint['segmentation_mask'], (PICTURE_SIZE, PICTURE_SIZE), method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
 
-  #normalize
+  #normalisieren (255 -> 1)
   input_image = tf.cast(input_image, tf.float32) / 255.0
   input_mask = tf.math.round(tf.cast(input_mask, tf.float32) / 255.0)
   inverted_mask = tf.math.subtract(tf.ones((PICTURE_SIZE, PICTURE_SIZE, 1)), input_mask)
 
   return input_image, inverted_mask
 
+#Hyperparameter
 TRAIN_LENGTH = info.splits['train'].num_examples
 BATCH_SIZE = 8
 BUFFER_SIZE = 50
@@ -34,7 +37,7 @@ print(STEPS_PER_EPOCH*BATCH_SIZE)
 print(TRAIN_LENGTH)
 print(info.splits['test'].num_examples)
 
-
+#Methode zum Visualieren von Bildern
 def display(display_list, with_colorbar=False):
   plt.figure(figsize=(15, 15))
 
@@ -57,6 +60,7 @@ for image, mask in train.take(1):
 train = train.shuffle(BUFFER_SIZE).batch(BATCH_SIZE).repeat(10)
 test = test.shuffle(BUFFER_SIZE).batch(BATCH_SIZE).repeat(10)
 
+#Model importieren
 model = getModel()
 print(model.summary())
 
@@ -85,6 +89,7 @@ EPOCHS = 3
 VAL_SUBSPLITS = 1
 VALIDATION_STEPS = info.splits['test'].num_examples//BATCH_SIZE//VAL_SUBSPLITS
 
+#Model trainieren
 model_history = model.fit(train, epochs=EPOCHS,
                           steps_per_epoch=STEPS_PER_EPOCH,
                           validation_steps=VALIDATION_STEPS,
@@ -97,6 +102,7 @@ val_loss = model_history.history['val_loss']
 
 epochs = range(EPOCHS)
 
+#Ausgabe des Verhalten der Loss-Function visualieren
 plt.figure()
 plt.plot(epochs, loss, 'r', label='Training loss')
 plt.plot(epochs, val_loss, 'bo', label='Validation loss')
@@ -107,5 +113,6 @@ plt.ylim([0, 1])
 plt.legend()
 plt.show()
 
+
 show_predictions(test, 6)
-model.save_weights('neuralnet/saved_weights')
+model.save_weights('neuralnet/saved_weights') #neuronales Netz speichern
