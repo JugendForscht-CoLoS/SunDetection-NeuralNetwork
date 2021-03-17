3051dbdbdc7f4edfefde601ba7cbe1f9042a4da4
Tim Jaeger <timjae@schuldorf.de>
2021-03-17 17:10:48 +0100

    Updated coremlConverter.py

--- coremlConverter.py
+++ coremlConverter.py
@@ -1,7 +1,7 @@
 import coremltools as ct
 import tensorflow as tf
 
-from model.model import getModel
+from model.unet import getModel
 from losses.losses import TverskyLoss
 
 pathML = input('Pfad des .mlmodels: ')
@@ -16,7 +16,9 @@ model.compile(optimizer=tf.keras.optimizers.Adam(lr=0.0005),
               metrics=['acc'])
 model.load_weights('neuronalnet/saved_weights')
 
-mlModel = ct.convert(model)
+image_input = ct.ImageType(shape = (1, 224, 224, 3), scale = 1 / 255, bias = [0, 0, 0], color_layout = "RGB")
+
+mlModel = ct.convert(model, inputs = [image_input])
 mlModel.author = author
 mlModel.license = modelLicense
 mlModel.short_description = description
