6f3527f362020f133bbeff7b4979ba81931bce80
Tim Jaeger <timjae@schuldorf.de>
2021-03-17 17:40:18 +0100

    update

--- coremlConverter.py
+++ coremlConverter.py
@@ -4,17 +4,17 @@ import tensorflow as tf
 from model.unet import getModel
 from losses.losses import TverskyLoss
 
-pathML = input('Pfad des .mlmodels: ')
-author = input('Autor: ')
-modelLicense = input('Lizenz: ')
-description = input('kurze Beschreibung: ')
+pathML = "/Users/timjaeger/Desktop/SunDetector.mlmodel"
+author = "Jonas Riemann"
+modelLicense = "MIT-License (https://github.com/JugendForscht-CoLoS/SunDetection-NeuralNetwork/blob/main/LICENSE)"
+description = "Ein neuronales Netz zum Erkennen der Sonne."
 version = input('Version: ')
 
 model = getModel()
 model.compile(optimizer=tf.keras.optimizers.Adam(lr=0.0005),
               loss=TverskyLoss(alpha=0.9835, smooth=2e4),
               metrics=['acc'])
-model.load_weights('neuronalnet/saved_weights')
+model.load_weights('neuralnet/saved_weights')
 
 image_input = ct.ImageType(shape = (1, 224, 224, 3), scale = 1 / 255, bias = [0, 0, 0], color_layout = "RGB")
 
